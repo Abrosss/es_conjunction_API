ecffb6cf8ea4277bd6f948b1f4d97986b98277b4
Abrosss <nataroro5@gmail.com>
2022-12-02 23:34:32 +0600

    added error handling

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const [isLoaded, setIsLoaded] = useState(false)
 const [loading, setLoading] = useState(false)
 const [tableRequested, setTableRequested] = useState(false)
 const [currentWord, setCurrentWord] = useState(null)
+const [error, setError] = useState(null)
 const [allWords, setAllWords] = useState([])
 const [isRoot, setIsRoot] = useState(false)
 console.log(result)
@@ -18,10 +19,18 @@ const input = useRef()
 
 let pronouns = ['yo', 'tú', "él/ella/usted", 'nosotros/nosotras', 'vosotros/vosotras', 'ellos/ellas/ustedes']
 let tenses = ['Present', 'Preterite', 'Imperfect', 'Conditional', 'Future']
-console.log(result)
+console.log(error)
 async function handleSubmit(e, word) {
- let root
   e.preventDefault()
+  setTableRequested(false)
+let array = word.split(' ')
+console.log(array)
+if(array.length > 1) {
+  
+  return setError('delete spaces or try again')
+} 
+ let root
+
   setIsLoaded(false)
   setLoading(true)
   await axios.get(`http://localhost:5000/admin/verbs/${word}`).then((response) => {
@@ -36,7 +45,9 @@ async function handleSubmit(e, word) {
       root = response.data[0].meaning[0].infinitive
       setIsRoot(false)
     } 
-  });
+  }).catch(err => {
+    if(err) setError('No word found. Check the spelling')
+  })
   await axios.get(`http://localhost:5000/admin/roots/${root}`).then((response) => {
     setAllWords(response.data)
     setIsLoaded(true)
@@ -81,12 +92,16 @@ function filter(pronoun, mood, tense) {
     <header>
       <h1>Spanish Verbs</h1>
     <form>
-      <input ref={input} autoComplete='off' onChange={(e) => setCurrentWord(e.target.value) } placeholder='spanish word here' name='word'></input>
+      <input ref={input} autoComplete='off' onChange={(e) => {
+        setCurrentWord(e.target.value)
+        setError(null)
+        }} placeholder='spanish word here' name='word'></input>
       <button onClick={(e) => handleSubmit(e, currentWord)}><img src={YourSvg}></img></button>
       </form>
     </header>
 
     <main>
+    {error && <span>{error}</span>}
     {loading &&
     <section className='table'>
 <lord-icon
@@ -100,6 +115,7 @@ function filter(pronoun, mood, tense) {
     }
       {isLoaded && result.map(word => (
         <section className='table'>
+          
           {!isRoot ?
            <p className='pronoun'>{capitalizeFirstLetter(word.meaning[0].performer)} <span className='selected'>{word.word}</span> </p> :
            <p className='pronoun dotted' onClick={() => setTableRequested(!tableRequested)} ><span className='selected'>{capitalizeFirstLetter(word.word)}</span> </p>
