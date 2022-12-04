95388da9ae5df65e5d135bac4275f75f1c278a51
Abrosss <nataroro5@gmail.com>
2022-12-04 11:21:55 +0600

    backend

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const fs = require('fs');
-const adminRoutes = require("./routes/admin");
+const apiRoutes = require("./routes/api");
 const connectDB = require("./config/database");
 const cors = require('cors')
 
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use("/admin", adminRoutes);
+app.use("/api", apiRoutes);
 //Server Running
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
--- backend/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const adminController = require("../controllers/admin");
-
-router.get("/verbs/:verb", adminController.getVerbs);
-router.get("/roots/:verb", adminController.oneRoot);
-
-
-module.exports = router;
